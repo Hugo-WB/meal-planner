339bbac9f8011e1a997f829c0798098b6ac336f8
Hugo Wong-Berard <hugowong71@gmail.com>
2020-08-25 22:44:10 +0100

    push

--- src/reducers/store.ts
+++ src/reducers/store.ts
@@ -1,7 +1,34 @@
 import { combineReducers, createStore } from "redux";
-import { firebaseReducer } from "react-redux-firebase";
-import { firestoreReducer } from "redux-firestore";
+import { firebaseReducer,FirebaseReducer } from "react-redux-firebase";
+import { firestoreReducer} from "redux-firestore";
 import localData from "./localData";
+
+// interface Recipe {
+//   name:string,
+//   id:string,
+//   description:string,
+//   imageSrc:string,
+//   owner:any, // <-- Fix this to whatever
+//   ingredients:string[],
+//   takeAway:boolean,
+// }
+
+// interface user{
+//   name:string,
+//   id:string,
+//   username?:string,
+//   password?:string,
+// }
+
+// interface Schema {
+//   recipes: Recipe,
+//   users: user,
+// }
+
+// interface RootState {
+//   firebase: FirebaseReducer.Reducer<Schema>,
+//   firestore:any, 
+// }
 
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
