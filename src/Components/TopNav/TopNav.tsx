973ce0f1727c5312bcf2250cfb81dfa7226f6627
Hugo Wong-Berard <hugowong71@gmail.com>
2020-08-26 09:31:16 +0100

    started work on add new recipe modal

--- src/Components/TopNav/TopNav.tsx
+++ src/Components/TopNav/TopNav.tsx
@@ -1,38 +1,49 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
+import {useFirestore} from 'react-redux-firebase'
 import { Menu, Button, Input } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
-export default class Topnav extends Component {
-  render() {
-    return (
-      <div>
-        <Menu color = "teal" pointing secondary>
-          <Menu.Item>
-            <img />
-          </Menu.Item>
-          <Link to="/dashboard" >
-            <Menu.Item link active = {true}>Dashboard</Menu.Item>
-          </Link>
-          <Link to="/plan">
-            <Menu.Item link>Plan</Menu.Item>
-          </Link>
-          <Link to="/recipes">
-            <Menu.Item link>Recipes</Menu.Item>
-          </Link>
-          <Menu.Menu position="right">
-            <Menu.Item>
-              <Input icon='search' placeholder='Search...' />
-            </Menu.Item>
-            <Menu.Item>
-              <Button>Add Recipe</Button>
-            </Menu.Item>
-            <Menu.Item position="right">
-              <Button>Sign Out</Button>
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu>
-        
-      </div>
-    )
-  }
+
+interface Props {
+  
 }
+
+const TopNav = (props: Props) => {
+  const firestore = useFirestore()
+  
+  const [open,setOpen] = useState(false) //For the Modal for creating a new recipe
+
+
+  return (
+    <div>
+      <Menu color = "teal" pointing secondary>
+        <Menu.Item>
+          <img />
+        </Menu.Item>
+        <Link to="/dashboard" >
+          <Menu.Item link active = {true}>Dashboard</Menu.Item>
+        </Link>
+        <Link to="/plan">
+          <Menu.Item link>Plan</Menu.Item>
+        </Link>
+        <Link to="/recipes">
+          <Menu.Item link>Recipes</Menu.Item>
+        </Link>
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Input icon='search' placeholder='Search...' />
+          </Menu.Item>
+          <Menu.Item>
+            <Button>Add Recipe</Button>
+          </Menu.Item>
+          <Menu.Item position="right">
+            <Button>Sign Out</Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+      
+    </div>
+  )
+}
+
+export default TopNav
