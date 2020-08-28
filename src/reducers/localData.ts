3c7deda4df1c6b9947bff833e4a87654496cdf58
Hugo Wong-Berard <hugowong71@gmail.com>
2020-08-28 22:35:38 +0100

    added autmatic redirect for unauthenticated users

--- src/reducers/localData.ts
+++ src/reducers/localData.ts
@@ -4,19 +4,32 @@ interface User {
 }
 interface LocalData {
   currentPage: string;
-  user?: User;
+  loggedIn:boolean;
+  user: User;
 }
 const initialState: LocalData = {
   currentPage: "Dashboard",
+  loggedIn:false,
+  user:{
+    uid:"",
+    username:"",
+  }
 };
 
 export default (state = initialState, action: any): LocalData => {
   switch (action.type) {
-    case "updateUser":
+    case "loginUser":
       return {
         ...state,
+        loggedIn:true,
         user: action.user,
       };
+    case "logoutUser":
+      return{
+        ...state,
+        loggedIn:false,
+        user:initialState.user,
+      }
     default:
       return state;
   }
--- src/views/Plan/Plan.tsx
+++ src/views/Plan/Plan.tsx
@@ -1,64 +1,85 @@
-import React, { useState } from 'react'
-import TopNav from "./../../Components/TopNav/TopNav"
+import React, { useState } from "react";
+import TopNav from "./../../Components/TopNav/TopNav";
 
-import FullCalendar from "@fullcalendar/react"
-import timeGridPlugin from "@fullcalendar/timegrid"
-import { Container, Form, Button, Segment, Grid } from 'semantic-ui-react'
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { Container, Form, Button, Segment, Grid } from "semantic-ui-react";
 
-import { useFirestoreConnect } from 'react-redux-firebase'
-import { useSelector } from 'react-redux'
-import * as firebase from "firebase"
-import "firebase/firestore"
+import { useFirestoreConnect, useFirestore } from "react-redux-firebase";
+import { useSelector } from "react-redux";
+// import * as firebase from "firebase"
+// import "firebase/firestore"
 
-import { RootState } from '../../reducers/store'
+import { RootState } from "../../reducers/store";
+import { useHistory } from "react-router-dom";
 
-
+interface Event {
+  title: string;
+  start: string;
+  end: string;
+}
 const Plan = () => {
-  const db = firebase.firestore()
-  const localUser = useSelector((state:RootState)=>state.localData.user)
-  useFirestoreConnect([{collection:"users"}])
-  const users = useSelector((state:RootState)=>state.firestore.data.users)
-  const [events,setEvents] = useState([])
-  if((users!==undefined)&& (localUser!==undefined)){
-    if (users[localUser.uid].events !== undefined){
-      setEvents(users[localUser.uid].events)
+  const firestore = useFirestore();
+  const history = useHistory();
+
+  const [events, setEvents] = useState<Event[]>([]);
+  const localUserUID = useSelector((state: RootState) => state.localData.user.uid);
+  useFirestoreConnect([{ collection: "users" }]);
+  const users = useSelector((state: RootState) => state.firestore.data.users);
+  // if ((localUserUID.length>1) && (users[localUserUID].events !== undefined) && (events.length<1)){
+  //   setEvents(users[localUserUID].events);
+  // }
+  const generateEvents = () => {
+    console.log("generating events")
+    setEvents([
+      {
+        title: "test",
+        start: "2020-08-28T17:21",
+        end: "2020-08-28T16:00",
+      },
+    ]);
+    if (localUserUID !== undefined) {
+      firestore.collection("users").doc(localUserUID).update({
+        events:events
+      }).then(()=>{
+        console.log("successfull, added events to firestore")
+        setEvents(events)
+      }).catch((error)=>{alert(error.message)});
+    } else {
+      alert("please login");
+      history.push("/");
     }
-  }
-
-  const generateEvents = () =>{
-
-  }
-
+  };
 
   const calendar = (
-    <Container style={{marginTop:"20px"}}>
-      <FullCalendar 
+    <Container style={{ marginTop: "20px" }}>
+      <FullCalendar
         plugins={[timeGridPlugin]}
         initialView="timeGridWeek"
-        events = {events}
+        events={events}
       />
     </Container>
-  )
+  );
 
   const form = (
     <Grid textAlign="center" verticalAlign="middle">
-      <Grid.Column style={{marginTop:"30px",maxWidth:"600px"}}>
-      <Segment stacked>
-        <Form>
-          <Button color="teal" onClick={generateEvents}>Generate</Button>
-        </Form>
-      </Segment>
+      <Grid.Column style={{ marginTop: "30px", maxWidth: "600px" }}>
+        <Segment stacked>
+          <Form>
+            <Button color="teal" onClick={generateEvents}>
+              Generate
+            </Button>
+          </Form>
+        </Segment>
       </Grid.Column>
     </Grid>
-  )
+  );
 
   return (
     <div>
       <TopNav />
-      {(events.length<1)?form:calendar}
-
-      
+      {/* {events.length < 1 ? form : calendar} */}
     </div>
-  )
-}
-export default Plan
+  );
+};
+export default Plan;
