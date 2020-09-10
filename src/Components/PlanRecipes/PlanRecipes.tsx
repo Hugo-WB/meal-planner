8de3e8f2a7b7e4b61095c16bb3df9c2143012288
Hugo Wong-Berard <hugowong71@gmail.com>
2020-09-10 18:34:35 +0100

    polishing touches

--- src/Components/PlanRecipes/PlanRecipes.tsx
+++ src/Components/PlanRecipes/PlanRecipes.tsx
@@ -1,5 +1,5 @@
-//The recipe cards in the Recipe page, as well as the modal(popup) for each of the recipes
-import React, { useState, ReactElement } from "react";
+// Creates the popup for the recipes in the plan/calendar
+import React, { useState, ReactElement, useEffect } from "react";
 import {
   Card,
   Image,
@@ -10,7 +10,7 @@ import {
   Icon,
 } from "semantic-ui-react";
 
-interface Props {
+interface Recipe {
   name: string;
   imageSrc: string;
   category?: string;
@@ -19,34 +19,41 @@ interface Props {
   steps: string;
   author: string;
 }
+interface Props{
+  clicks:number,
+  recipe:Recipe,
+}
 
 export default function RecipeCard(props: Props): ReactElement {
-  const [showRecipe, setShowRecipe] = useState(false);
+  const [showRecipe, setShowRecipe] = useState(true);
   const [showUser, setShowUser] = useState(false);
+  useEffect(()=>{
+    setShowRecipe(true)
+  },[props.clicks])
 
   const Recipe = (
     <Modal open={showRecipe} closeIcon onClose={() => setShowRecipe(false)}>
       <Modal.Header>
         <Grid columns="equal" textAlign="center" container>
-          <Grid.Column>{props.name}</Grid.Column>
+          <Grid.Column>{props.recipe.name}</Grid.Column>
           <Grid.Column textAlign="right">
             <p onClick={() => setShowUser(true)} style={{ cursor: "pointer" }}>
-              {props.author}
+              {props.recipe.author}
             </p>
           </Grid.Column>
         </Grid>
       </Modal.Header>
       <Modal.Content image>
-        <Image size="medium" src={props.imageSrc} />
+        <Image size="medium" src={props.recipe.imageSrc} />
         <Modal.Description>
           <Header>Ingredients:</Header>
           <List bulleted>
-            {props.ingredients.map((ingredient) => (
+            {props.recipe.ingredients.map((ingredient) => (
               <List.Item>{ingredient}</List.Item>
             ))}
           </List>
           <Header>Steps:</Header>
-          <p>{props.steps}</p>
+          <p>{props.recipe.steps}</p>
         </Modal.Description>
       </Modal.Content>
     </Modal>
@@ -54,24 +61,15 @@ export default function RecipeCard(props: Props): ReactElement {
 
   const User = (
     <Modal open={showUser} onClose={() => setShowUser(false)} closeIcon>
-      <Modal.Header>{props.author}</Modal.Header>
+      <Modal.Header>{props.recipe.author}</Modal.Header>
     </Modal>
   );
 
   return (
-    <Card onClick={() => setShowRecipe(true)} style={{ margin: "20px" }}>
-      <Image src={props.imageSrc} />
-      <Card.Content>
-        <Card.Header>{props.name}</Card.Header>
-        <Card.Meta>{props.category}</Card.Meta>
-        <Card.Description>{props.description}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Icon name="user" color="blue" />
-        {props.author}
-      </Card.Content>
+    <div>
       {Recipe}
       {User}
-    </Card>
+
+    </div>
   );
 }
